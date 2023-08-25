8cfaeb70718e7c6e274911eee580e5b218319107
Лилия <l_loshakova123@mail.ru>
2023-08-25 23:17:18 +0300

    Sketch

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-new Vue({
-    el: '#app',
-})
+
 
 Vue.component('birthday', {
     data() {
         return {
-
+            title: 'Birthday invitation',
+            header__text: 'Приглашение на день рождения',
+            content__text: 'Дорогие друзья! Приглашаю вас на торжество, посвященное моему дню рождению. Хочу в этот день видеть рядом самых близких и родных людей.Задор и веселье гарантирую!',
+            content__address: 'Жду вас 24.09.23г. в 15 часов на улице Котовского 121.',
+            content__withMyself: 'С собой иметь хорошее настроение и парочку тостов в мою честь. Отказы не принимаются.',
+            img_one: 'img/img1.svg',
+            img_two: 'img/img2.svg'
         }
     },
 
     template: `<section class="header">
     <div class="main">
-        <h1 class="header__title">Birthday invitation</h1>
-        <p class="header__text">Приглашение на день рождения</p>
+        <h1 class="header__title">{{ title }}</h1>
+        <p class="header__text">{{ header__text }}</p>
         <article class="content">
-            <p class="content__text">Дорогие друзья! Приглашаю вас на торжество, посвященное моему дню рождению.
-                Хочу в этот день видеть
-                рядом
-                самых близких и родных людей. Задор и веселье гарантирую! </p>
-            <p class="content__address">Жду вас 24.09.23г. в 15 часов на улице Котовского 121. </p>
-            <p class="content__withMyself">С собой иметь хорошее настроение и парочку тостов в мою честь. Отказы
-                не принимаются.</p>
+            <p class="content__text">{{ content__text }}</p>
+            <p class="content__address">{{ content__address }}</p>
+            <p class="content__withMyself">{{ content__withMyself }}</p>
         </article>
     </div>
     <article class="content__img">
-        <img class="photo" src="img/img1.svg" alt="flower">
-        <img class="photo" src="img/img2.svg" alt="flower">
+        <img class="photo" :src="img_one" alt="flower">
+        <img class="photo" src="" alt="flower">
     </article>
 </section>`,
 });
