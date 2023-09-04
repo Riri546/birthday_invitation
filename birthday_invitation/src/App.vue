d7b41f39b0e3d6c6879a91978d77a3865580a9f2
Лилия <l_loshakova123@mail.ru>
2023-09-04 15:09:43 +0300

    Sketch

--- birthday_invitation/src/App.vue
+++ birthday_invitation/src/App.vue
@@ -16,5 +16,131 @@ export default {
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 
+body {
+  background-color: #FFE3E4;
+  font-family: "Cormorant Garamond", serif;
+  font-family: "Tangerine", cursive;
+}
+
+a {
+  text-decoration: none;
+}
+
+.header .content__withMyself,
+.header .content__address,
+.header .content__text {
+  padding-bottom: 70px;
+  max-width: 500px;
+  color: #000;
+  text-align: center;
+  font-family: Cormorant Garamond;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+
+.main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.header {
+  margin: 60px 180px;
+  padding-top: 70px;
+  border: 5px solid #FF9094;
+  background: #FFF;
+}
+
+.header__title {
+  color: #000;
+  text-align: center;
+  font-family: Tangerine;
+  font-size: 150px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+}
+
+.header__text {
+  max-width: 500px;
+  padding-bottom: 52px;
+  color: #000;
+  text-align: center;
+  font-family: Cormorant Garamond;
+  font-size: 50px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
+
+.header .content__img {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+/* Стили для среднего телефона */
+@media (max-width: 1024px) {
+
+  .header .content__text,
+  .header .content__address,
+  .header .content__withMyself {
+    padding-bottom: 20px;
+    color: #000;
+    text-align: center;
+    font-family: Cormorant Garamond;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+
+  .body {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .header {
+    margin: 40px;
+    padding-top: 40px;
+    border: 2px solid #FF9094;
+    background: #FFF;
+  }
+
+  .header__title {
+    max-width: 280px;
+    color: #000;
+    text-align: center;
+    font-family: Tangerine;
+    font-size: 45px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
+
+  .header__text {
+    max-width: 300px;
+    padding-bottom: 30px;
+    color: #000;
+    text-align: center;
+    font-family: Cormorant Garamond;
+    font-size: 30px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+
+  .photo {
+    padding-top: 50px;
+    max-width: 120px;
+  }
+}
 </style>
